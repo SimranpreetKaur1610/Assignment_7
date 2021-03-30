84f356ed628947c55b617693238e8228b11fe74d
Simranpreetkaur <harsim610@gmail.com>
2021-03-30 14:33:37 -0400

    added methods and created new class TransactionDetails

--- src/com/accountDetails/BankAccount.java
+++ src/com/accountDetails/BankAccount.java
@@ -1,21 +1,96 @@
 package com.accountDetails;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class BankAccount {
 
     private String accountNo;
 
-    public BankAccount() {
+    public BankAccount(){
         this.accountNo = "";
     }
 
-    public BankAccount(String accountNumber) {
+    public BankAccount(String accountNumber){
         this();
         this.accountNo = accountNumber;
     }
 
-    public String getAccountNumber() {
+    public String getAccountNumber(){
         return this.accountNo;
     }
 
+    public static BankAccount open(String name) throws IOException {
+        String id = (int) (Math.random() * 10000)+"";
+        while(true){
+            if(!accountExists(id)){
+                File f = new File(id+".txt");
+                f.createNewFile();
+                FileWriter writer = new FileWriter(f);
+                writer.write(name+"-"+0);
+                writer.close();
+
+                return new BankAccount(id);
+            }
+            id = (int) (Math.random() * 10000)+"";
+        }
+    }
+
+    public String[] getAccountDetails() throws FileNotFoundException {
+        String[] details = {};
+        if(accountExists(this.accountNo)){
+            File file = new File(this.accountNo+".txt");
+            Scanner read = new Scanner(file);
+            String data = "";
+            while (read.hasNextLine()) {
+                data = read.nextLine().trim();
+                if(!data.equals(""))
+                    break;
+            }
+            details = data.split("-");
+        }
+
+        return details;
+    }
+
+    public void processTransaction(TransactionDetails transaction) throws IOException {
+        String[] details = getAccountDetails();
+        String username = details[0];
+        double balance = Double.parseDouble(details[1]);
+
+        double updatedBalance = balance + transaction.getAmount();
+
+        if(updatedBalance < 0){
+            System.out.println("Balance is not sufficient !");
+            return ;
+        }
+
+        File file = new File(this.accountNo+".txt");
+        FileWriter writer = new FileWriter(file, false);
+        writer.write(username+"-"+updatedBalance);
+        writer.close();
+    }
+
+    public static boolean accountExists(String accountNumber){
+        return new File(accountNumber + ".txt").exists();
+    }
+
+}
+
+class TransactionDetails{
+    private double amount;
+
+    public TransactionDetails(double amount){
+        this.amount = amount;
+    }
+
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+    public double getAmount(){
+        return this.amount;
+    }
 }
--- src/com/accountDetails/MainClass.java
+++ src/com/accountDetails/MainClass.java
@@ -1,9 +1,40 @@
 package com.accountDetails;
 
+import javax.swing.*;
+import java.io.IOException;
+import static java.lang.Double.isNaN;
+
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        start();
 
     }
 
+    public static void start() throws IOException {
+        BankAccount account;
+        int input = JOptionPane.showConfirmDialog(null, "Do you have an Account ? \n\nClick No to create one otherwise Yes");
+        if(input == JOptionPane.NO_OPTION){
+            account = openNewAccount();
+        }else if(input == JOptionPane.YES_OPTION){
+            account = new BankAccount(getAccountNumber());
+        }else{
+            return;
+        }
+
+        if(account.getAccountDetails().length < 2 ) {
+            JOptionPane.showMessageDialog(null, "Account number is invalid! Try Again!");
+            start();
+        }
+
+    }
+
+    public static String getAccountNumber(){
+        return JOptionPane.showInputDialog("Enter your account number");
+    }
+
+    public static BankAccount openNewAccount() throws IOException {
+        String name = JOptionPane.showInputDialog("Enter your name").trim();
+        return BankAccount.open(name);
+    }
 }
